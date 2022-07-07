29ddd9b416fe6d514bc46730b2c27bab083a2d75
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-07-07 16:58:36 -0300

    more work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists#index'
+  # devise_for :users
+  root to: 'pages#home'
+
   resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: :create
